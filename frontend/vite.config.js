83b95a159e031b4485d1829300ff759a5867a3e7
harsh patil <1234harshpatil@gmail.com>
2024-08-04 22:23:32 +0530

    commit changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api/v1' : 'https://mern-trello-task-management.onrender.com',
+      '/api/v1' : 'https://mern-trello-task-management-frontend.onrender.com',
     }
   },
   plugins: [react()],
